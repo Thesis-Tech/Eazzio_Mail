0688442867339a9d14794950e0796c5110e06fbe
KumarRahul001 <kumarrahulraj468@outlook.com>
2026-08-31 22:05:58 +0530

    feat(web): add /mail/archive route and remove empty auth directory

---
+++ apps/web/src/app/mail/archive/page.tsx
@@ -0,0 +1,8 @@
+'use client';
+
+import React from 'react';
+import { MailDashboard } from '../../../components/mail/MailDashboard';
+
+export default function ArchivePage() {
+  return <MailDashboard initialFolder="fld-archive" />;
+}
